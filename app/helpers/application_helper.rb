dd8b6d1c5cbe1648785eeaf1d19d79aa49e8a0a1
= <=>
2021-04-22 19:21:16 +0530

    changes for white list tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def remove_html_content(data)
 		sanitizer_obj = Rails::Html::SafeListSanitizer.new
-		sanitizer_obj.sanitize(data, tags: %w(b br ul li p))
+		sanitizer_obj.sanitize(data, tags: %w(b br ul li p img), attributes: %w(src srcset))
 	end
 end
